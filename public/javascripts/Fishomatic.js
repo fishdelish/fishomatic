7afb930023686dad592ee148b62a0bb19b113325
David Workman <workmad3@gmail.com>
2011-02-01 21:30:33 +0000

    Added js snippet

---
+++ public/javascripts/Fishomatic.js
@@ -0,0 +1,5 @@
+$("fish-file").each(function(index, item) {
+  var current_url = $(item).attr("href");
+  var new_url = "http://fishdelish.cs.man.ac.uk/fishomatic/fish_files/display?fish_file="+current_url
+  $(item).attr("href", new_url);
+});
