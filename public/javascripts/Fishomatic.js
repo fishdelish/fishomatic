7c39f6c46679c3dd3fb26f86bf6981787c476205
David Workman <workmad3@gmail.com>
2011-02-01 21:38:46 +0000

    Messed up the selector

--- public/javascripts/Fishomatic.js
+++ public/javascripts/Fishomatic.js
@@ -1,6 +1,5 @@
-$("fish-file").each(function(index, item) {
+$(".fish-file").each(function(index, item) {
   var current_url = $(item).attr("href");
   var new_url = "http://fishdelish.cs.man.ac.uk/fishomatic/fish_files/display?fish_file="+current_url
-  alert("Setting " + current_url + " to " + new_url);
   $(item).attr("href", new_url);
 });
