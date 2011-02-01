0c26f94cf7c57deec79774fba3745684eca1d0f9
David Workman <workmad3@gmail.com>
2011-02-01 21:35:59 +0000

    Added a trace

--- public/javascripts/Fishomatic.js
+++ public/javascripts/Fishomatic.js
@@ -1,5 +1,6 @@
 $("fish-file").each(function(index, item) {
   var current_url = $(item).attr("href");
   var new_url = "http://fishdelish.cs.man.ac.uk/fishomatic/fish_files/display?fish_file="+current_url
+  alert("Setting " + current_url + " to " + new_url);
   $(item).attr("href", new_url);
 });
