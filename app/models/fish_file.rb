e1adb2ab440c39bbc1c264fa326f90cc4e186f1f
David Workman <workmad3@gmail.com>
2011-02-01 17:36:57 +0000

    Added map view and first attempt to fix URI issue

--- app/models/fish_file.rb
+++ app/models/fish_file.rb
@@ -4,7 +4,12 @@ class FishFile < ActiveRecord::Base
 
   def data
     @file = open(self.fish_file.path)
-    @file.read
+    @content = @file.read
+    @content.gsub!(/<fish:species>(.*)<\/fish:species>/) do |match|
+      species_uri = "http://fishdelish.cs.man.ac.uk/rdf/species/" + $1.gsub(/ /, "/")
+      species_tag = "<fish:speciesURI>http://fishdelish.cs.man.ac.uk/species/species.php?uri=#{@species_uri}</fish:speciesURI>"
+      match + species_tag
+    end
   end
 
   def name
---
+++ config/initializers/map_keys.rb
@@ -0,0 +1,1 @@
+KEYS = YAML.load_file(Rails.root + "config/map_keys.yml")[Rails.env]
