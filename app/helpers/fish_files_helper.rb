e43eb99d84f2cc552a0f91b2395af102eee1def2
David Workman <workmad3@gmail.com>
2011-02-22 21:02:59 +0000

    Minor refactoring and added remaining fields of fish observations to the exhibit display

--- app/helpers/fish_files_helper.rb
+++ app/helpers/fish_files_helper.rb
@@ -1,2 +1,21 @@
 module FishFilesHelper
+  def fish_file_fields
+    [
+      {:field_name => ".date", :field_label => "Observation date"},
+      {:field_name => ".remark", :field_label => "Remarks"},
+      {:field_name => ".FAO_area", :field_label => "Area"},
+      {:field_name => ".country", :field_label => "Country"},
+      {:field_name => ".locality", :field_label => "Locality"},
+      {:field_name => ".lat_lng", :field_label => "Location (lat, long)"},
+      {:field_name => ".accuracy", :field_label => "Location accuracy"},
+      {:field_name => ".abundance", :field_label => "Species abundance"},
+      {:field_name => ".type", :field_label => "Species type"},
+      {:field_name => ".gear", :field_label => "Gear used"},
+      {:field_name => ".temperature", :field_label => "Temperature"},
+      {:field_name => ".day_time", :field_label => "Day time"},
+      {:field_name => ".life_stage", :field_label => "Life stage"},
+      {:field_name => ".length", :field_label => "Fish length"},
+      {:field_name => ".salinity", :field_label => "Water salinity"}
+    ]
+  end
 end
